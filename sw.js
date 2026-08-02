4e32568f1077d30ca46eca2ab19cae772a2bb9be
fiksiverse <fiksiverse@gmail.com>
2026-08-02 15:06:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
+self.addEventListener('install', (e) => {[span_60](start_span)[span_60](end_span)
+  self.skipWaiting();[span_61](start_span)[span_61](end_span)
+});[span_62](start_span)[span_62](end_span)
 
-self.addEventListener('fetch', (event) => {
-  // Service Worker dasar untuk syarat PWA
-});
+self.addEventListener('fetch', (event) => {[span_63](start_span)[span_63](end_span)
+  // Service Worker dasar untuk syarat PWA[span_64](start_span)[span_64](end_span)
+});[span_65](start_span)[span_65](end_span)
